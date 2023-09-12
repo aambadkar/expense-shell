540a49224bda6ade0fb4d14a46423be190a19ec0
aambadkar <khodaskar.ankita@gmail.com>
2023-09-12 19:35:50 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -43,6 +43,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 
 fi
 
@@ -57,6 +58,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 
 fi
 
@@ -68,6 +70,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 
 fi
 
@@ -77,6 +80,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 
 fi
 
@@ -86,5 +90,6 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mSUCCESS\e[0m"
 else
   echo -e "\e[31mFAILED\e[0m"
+  exit
 
 fi
