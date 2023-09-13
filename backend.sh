59458a696a4a2f60c4691ff39dd045a80cf9bc61
aambadkar <khodaskar.ankita@gmail.com>
2023-09-13 23:56:25 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -3,48 +3,23 @@ component=backend
 
 echo install nodJS Repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo install nodeJS
 dnf install nodejs -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo add application user
 useradd expense &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo clean app contend
 rm -rf /app &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 mkdir /app
 cd /app
@@ -53,38 +28,18 @@ download_and_extract
 
 echo download dependencies
 npm install &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo install mysql client
 dnf install mysql -y &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
 
 echo load the schema
 mysql -h mysql.devopsa17.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit 1
-fi
+stat_check
--- common.sh
+++ common.sh
@@ -1,21 +1,19 @@
 log_file=/tmp/expense.log
 
 download_and_extract() {
-echo download $component code
-curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit
-fi
+ echo download $component code
+  echo $?
 
-echo extracting $component code
-unzip /tmp/$component.zip >>$log_file
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILED\e[0m"
-  exit
-fi
+ echo extracting $component code
+ unzip /tmp/$component.zip >>$log_file
+ echo $?
+}
+
+stat_check(){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILED\e[0m"
+    exit 1
+  fi
 }
