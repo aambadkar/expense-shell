546a96ec01807c4ac3fd65ed9ffd0a7bb90ea33e
aambadkar <khodaskar.ankita@gmail.com>
2023-09-14 00:17:56 +0530

    expense script

--- backend.sh
+++ backend.sh
@@ -14,7 +14,10 @@ dnf install nodejs -y &>>$log_file
 stat_check
 
 echo add application user
-useradd expense &>>$log_file
+id expense &>>log_file
+if [ $? -ne 0 ]; then
+  useradd expense &>>log_file
+fi
 stat_check
 
 echo clean app contend
